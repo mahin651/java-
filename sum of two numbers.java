9dd2b2295dc2bf4168b49f52353c0d23b6852fdd
mahin651 <53258123+mahin651@users.noreply.github.com>
2020-07-27 12:10:55 +0530

    Create sum of two numbers.java

---
+++ sum of two numbers.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class sum{
+
+     public static void main(String []args){
+          Scanner sc = new Scanner(System.in);
+          System.out.println("enter first number");
+          int x = sc.nextInt();
+          System.out.println("enter second number");
+          int y = sc.nextInt();
+                  System.out.println("numbers entred by user are"+" "+x+" " +y);
+                int sum =x+y;
+
+
+                System.out.println("result after addition is"+" "+sum);
+
+
+
+     }
+}
+
