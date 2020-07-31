4ebed2c1e5bf0095c902f4668dadd63a40f227dc
mahin651 <53258123+mahin651@users.noreply.github.com>
2020-07-31 18:47:39 +0530

    Create thiskeyword.java

---
+++ thiskeyword.java
@@ -0,0 +1,26 @@
+public class Account{
+int a;
+int b;
+
+ public void setData(int a ,int b){
+  a = a;
+  b = b;
+ }
+  public void setDatathis(int a ,int b){
+this.a =a;
+this.b =b;
+}
+ public void showData(){
+   System.out.println("Value of A ="+a);
+   System.out.println("Value of B ="+b);
+ }
+ public static void main(String args[]){
+   Account obj = new Account();
+   obj.setData(2,3);
+   obj.showData();
+   obj.setDatathis(2,3);
+    obj.showData();
+ 
+     
+ }
+}
