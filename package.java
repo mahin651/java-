4536187c1136157f753411c2a7bcc3048f81d8f8
mahin651 <53258123+mahin651@users.noreply.github.com>
2020-07-31 18:49:57 +0530

    Create package.java

---
+++ package.java
@@ -0,0 +1,17 @@
+import java.util.Vector; 
+   
+public class ImportDemo 
+{ 
+   public ImportDemo() 
+   { 
+
+      Vector newVector = new Vector(); 
+   
+      java.util.ArrayList newList = new java.util.ArrayList(); 
+   } 
+   
+   public static void main(String arg[]) 
+   { 
+      new ImportDemo(); 
+   } 
+} 
